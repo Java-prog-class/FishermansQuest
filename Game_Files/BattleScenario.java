f3eb44903ee02c923eedc7977f738c3fd7d6bdb0
BaileyCollins <bailey.collins@hotmail.com>
2014-12-04 17:46:08 -0500

    Create BattleScenario.java

---
+++ Game_Files/BattleScenario.java
@@ -0,0 +1,33 @@
+public class BattleScenario{
+	
+	//Method for highest speed stat goes first.
+		
+	//Random method for critical hit if random 
+	//number is higher than 90 damage times 2.
+	
+	//Method for amount of armour reduces the 
+	//total damage done by the hit. 
+	//(if armour is at 50/100 damage reduction is 25%)
+		
+	//Call up character values
+	//	-Heath
+	//  -Defence (including armour values)
+	//	-Speed
+	//  -Names
+	//  -Attack (including weapon values)
+		
+	//Keep track of and display
+	//	-Character Name
+	//	-Health
+		
+	//Each turn call up
+	//	-whose turn
+	//  -if anyone is dead
+	
+	//when turn is decided player can choose
+	//	-weapon for attack
+	//	-type of attack
+	//	-escape
+	//	-
+
+}
