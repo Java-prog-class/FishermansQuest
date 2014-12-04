06152f3e1600ec5e41886a893b1734436463369c
BaileyCollins <bailey.collins@hotmail.com>
2014-12-04 18:14:40 -0500

    Update Items.java

--- Game_Files/Items.java
+++ Game_Files/Items.java
@@ -1,3 +1,49 @@
 public class Items{
 
+  //2-d arrays with the items inside each item category
+	
+	//main array ItemCat
+	//	-FoodAndDrink
+	//	-Weapons
+	//	-Armour
+	//	-Keys
+	//	-Misc
+	
+	//Inside FoodAndDrink
+	//	-Sandwich
+	//	-Raw Fish
+	//	-Cooked Fish
+	//	-Water
+	//	-Cheese
+	//	-Tea
+	//	-Coffee
+	//	-Bread
+	//	-Hot Peppers
+	
+	//Inside Weapons
+	//	-Kitchen Knife
+	//	-Harpoon Gun
+	//	-Damascus Sword
+	//	-Battle Axe
+	//	(Possible Bow and Arrows?)
+	
+	//Inside Armour
+	//	-Helmet
+	//	-ChainMail
+	//  -ChestPlate
+	//	-PlateLegs
+	//	-Shield
+	//	(Anti-Fire Shield?)
+	
+	//Inside Keys
+	//	-Crypt Key
+	//	-Tower Key
+	
+	//Inside Misc
+	//	-Books
+	//	-GasLamp(Inside Crypt To Burn Remains)
+	//	-Lantern
+	//	-Dragon Head
+	
+
 }
