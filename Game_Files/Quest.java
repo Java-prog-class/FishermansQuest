31e7ca855060478272d081312f7ed80e2adbeb25
Soap696 <ryanfagan455@gmail.com>
2014-12-15 14:02:39 -0500

    Update Quest.java

--- Game_Files/Quest.java
+++ Game_Files/Quest.java
@@ -3,8 +3,39 @@
 public class Quest{
 
 	public static void main(String[] Args){
-	
-	
+	//	for (Room m : roomList){
+	//		System.out.println(m.toString());
+	//	}
+	}
+	//Items(String name, int h, int am, int dm, boolean carry) {
+	void makeItems() {
+		Items z = new Items("Sandwich", 15, 0,0,true);
+		itemList.add(z);
+		z = new Items("banana", 5, 0,0,true);
+		itemList.add(z);
+		z = new Items("Cooked Fish", 25, 0, 0, true);
+		itemList.add(z);
+		z = new Items("Kitch Knife", 0, 10, 0, true);
+		itemList.add(z);
+		z = new Items("Harpoon Gun", 0, 50, 0, true);
+		itemList.add(z);
+		
+		
+		for (Items item : itemList){
+			System.out.println(item.name);
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
 	
 	}
 
