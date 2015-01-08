7d19b452a439a1b83f1cdbc38f9fe94450da3324
Soap696 <ryanfagan455@gmail.com>
2015-01-08 13:54:44 -0500

    Update room.java

--- Game_Files/room.java
+++ Game_Files/room.java
@@ -62,16 +62,23 @@ public class Room{
 		
 		r = new Room(2, "--Front of House--\n\n", "\nIt's old, looks abandoned, boarded up windows.");
 		r.setExits(3,0,1,0,0,0); 
-		r.long_desc = " ";
-		r.roomItems.add("Shattered windows with boards nailed to them, trees hug the\n sides of the house. Bird bath to the left of you.");
+		r.long_desc = "Old white house, paint looks chipped. There is no way around. Shattered windows with boards nailed to them, trees hug the\n sides of the house. Bird bath to the left of you.";
 		roomList.add(r);
 		
 		r = new Room(3, "--Living Room--\n\n", "\nYou walk in. Candles are lit around the room.");//Cabnet(needs key): Long Sword, Rope, Gas Lantern
 		r.setExits(0,4,2,5,0,0); 
+		r.long_desc = " ";
+		r.roomItems.add("Cabinet");
+		//figure out how to put things into a container
+		//r.roomItems.add("Long Sword");
+		//r.roomItems.add("Rope");
+		//r.roomItems.add("Gas Lantern");
 		roomList.add(r);
 		
 		r = new Room(4, "--Bedroom--\n\n", "\nDoor swings open. Half unpacked boxes are scattered all over the room.");//Boat Part 1, Cabnet key
 		r.setExits(0,0,0,3,0,0);
+		r.roomItems.add("Cabinet Key");
+		r.roomItems.add("Boat Part 1");
 		roomList.add(r);
 		
 		r = new Room(5, "--Hallway--\n\n", "\nNothing but the dim lighting of the candles.");
@@ -84,30 +91,45 @@ public class Room{
 		
 		r = new Room(7, "--Basement Hallway--\n\n", "\nDarkness. Large webs surround the wall.");//Tower key
 		r.setExits(0,0,9,6,0,0);
+		r.roomItems.add("Tower Key");
 		roomList.add(r);
 		
 		r = new Room(8, "--Attic--\n\n", "\nDusty, webs, crates. ");//Mace, Boat Part 2
 		r.setExits(0,0,0,0,0,6);
+		r.roomItems.add("Mace");
+		r.roomItems.add("Boat Part 2");
 		roomList.add(r);
 		
 		r = new Room(9, "--Crypt--\n\n", "--ENTERING COMBAT--");// Giant Spiders, Armor: helmet, leggings, chest plate, shield (after battle)
 		r.setExits(7,10,0,11,0,0);
+		r.roomItems.add("Iron Helmet");
+		r.roomItems.add("Chest Plate");
+		r.roomItems.add("Plate Leggings");
+		r.roomItems.add("shield");
 		roomList.add(r);
 		
 		r = new Room(10, "--Laboratory--\n\n", "\nA table with stuff on a table. Smashed beakers on the floor.");// Health Potion, Damage Rage Potion
 		r.setExits(0,0,0,9,0,0);
+		r.roomItems.add("Health Potion");
+		r.roomItems.add("Damage/Rage Potion");
 		roomList.add(r);
 		
 		r = new Room(11, "--Underground Pathway--\n\n", "\nLeads to an old door.");//need key to get in(tower key)
 		r.setExits(0,9,0,12,0,0);
+		r.roomItems.add("Tower Key");
 		roomList.add(r);
 		
 		r = new Room(12, "--Base of Tower--\n\n", "\nStairs are leading up to the top.");//Chest(needs key): Damascus Sword, Health potion
 		r.setExits(0,11,0,0,13,0);
+		r.roomItems.add("Chest");
+		//Figure out how to put these into the container
+		//r.roomItems.add("Damascus Sword");
+		//r.roomItems.add("Health Potion");
 		roomList.add(r);
 		
 		r = new Room(13, "--Top of Tower--\n\n", "--ENTER COMBAT--");// 3 headed dog, Boat Part 3
 		r.setExits(0,0,0,0,0,12);
+		r.roomItems.add("Boat Part 3");
 		roomList.add(r);
 		
 	//	for (Room m : roomList){
