fa320de1fbd8b27f4a4a61a097835cad8d4be2f4
Soap696 <ryanfagan455@gmail.com>
2015-01-14 14:46:33 -0500

    Update Items.java

--- Game_Files/Items.java
+++ Game_Files/Items.java
@@ -7,40 +7,16 @@ public class Items {
 	int attackModifier = 0;
 	int defenseModifier = 0;	
 	boolean isCarryable = true;
+	boolean inInventory = false;
 	//boolean isContainer = false;
 	
 	Items() {};  //empty constructor. Only used in loops
-	Items(String name, int h, int am, int dm, boolean carry) {
+	Items(String name, int h, int am, int dm, boolean carry, boolean mine) {
 		this.name = name;
 		this.health = h;
 		attackModifier = am;
 		defenseModifier = dm;
 		isCarryable = carry;
+		inInventory = mine;
 	}
-	//???? activate
-	/*
-	 type: food, weapon, armour, keys, misc
-	 Food
-	 	name = Sandwich
-	 	health = 10; increase health by 10
-	 	
-	 Weapons
-	 	name = Damascus Sword
-	 	attackModifier= 15
-	 Armour
-	  	name = chain mail
-	  	defenseModifier = 30
-	 Key
-	 	name = crypt key
-	 	open = which room or item it opens
-	 Misc
-	 	name = 
-	 	carryable = true; //can be carried in inventory. 
-	 				False means that it stays in the room
-	 	activate = ???
-	 */
-		
-
-	
-
 }
